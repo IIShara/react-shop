a9e39da1c1ff1d294f31520c35b0d4d5d0c2ca52
IIShara <i.sharafeev@inbox.ru>
2024-10-23 12:57:43 +0000

    update main URL

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
     return <nav className="blue-grey">
             <div className="nav-wrapper">
-            <a href="/" className="brand-logo">React Shop</a>
+            <a href="/react-shop/" className="brand-logo">React Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
                 <li><a href="https://github.com/IIShara/react-shop" target="_blank">Repo</a></li>
             </ul>
